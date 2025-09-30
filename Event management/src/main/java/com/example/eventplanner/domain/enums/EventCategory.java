81dac494e9239a0aa89c6e04cceed4504f222aa7
it24100038 <it24100038@my.sliit.lk>
2025-09-30 23:18:33 +0530

    Create EventCategory.java

---
+++ Event management/src/main/java/com/example/eventplanner/domain/enums/EventCategory.java
@@ -0,0 +1,5 @@
+// domain/enums/EventCategory.java
+package com.example.eventplanner.domain.enums;
+public enum EventCategory {
+    WEDDING, CONCERT, CONFERENCE, BIRTHDAY, CORPORATE, SEMINAR, WORKSHOP, OTHER
+}
