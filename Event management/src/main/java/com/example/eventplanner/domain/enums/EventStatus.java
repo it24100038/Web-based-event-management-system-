5de193126b33c4efeb84f66008d0e62aa63c5f44
it24100038 <it24100038@my.sliit.lk>
2025-09-30 23:18:53 +0530

    Add files via upload

---
+++ Event management/src/main/java/com/example/eventplanner/domain/enums/EventStatus.java
@@ -0,0 +1,5 @@
+// domain/enums/EventStatus.java
+package com.example.eventplanner.domain.enums;
+public enum EventStatus {
+    DRAFT, PENDING, PUBLISHED, REJECTED, CANCELLED, COMPLETED
+}
---
+++ Event management/src/main/java/com/example/eventplanner/domain/enums/Role.java
@@ -0,0 +1,3 @@
+// domain/enums/Role.java
+package com.example.eventplanner.domain.enums;
+public enum Role { ADMIN, PLANNER }
